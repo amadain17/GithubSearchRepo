46b4f9c3c19db0493f5066630920a21cbe1b5b8f
Mark Devine <mfmdevine@gmail.com>
2021-02-16 04:19:02 +0000

    Added .gitignore file, altered Readme to give better instructions, altered pom

---
+++ src/main/java/ie/mfmdevine/GithubSearchRepo/GithubSearchRepo.java
@@ -0,0 +1,13 @@
+package ie.mfmdevine.GithubSearchRepo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubSearchRepo {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GithubSearchRepo.class, args);
+	}
+
+}
